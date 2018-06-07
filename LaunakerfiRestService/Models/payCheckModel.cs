60c1ca275e3d41467f5e5cf65cfdb78768b22ee8
Elsa Einasrdóttir <elsam@simnet.is>
2018-06-07 14:13:11 +0000

    First checkin

---
+++ LaunakerfiRestService/Models/payCheckModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LaunakerfiRestService.Models
+{
+    public class payCheckModel
+    {
+        public int Id { get; set; }
+        public int IdLaunthegi { get; set; }
+        public byte[] launasedillprent { get; set; }
+        public DateTime timiFra { get; set; }
+        public DateTime timiTil { get; set; }
+        public int IdLaunagreidsla { get; set; }
+        public int IdRskSkilagrein { get; set; }
+        public int IdLifeyrirSkilagrein { get; set; }
+        public int astand { get; set; }
+    }
+
+    public class SalarySystemDBContext : DbContext
+    {
+        public DbSet<payCheckModel> PayCheck { get; set; }
+    }
+}
